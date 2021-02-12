abb207df6d44b616e24fad55bc65c7d85adc493d
ashumakov <>
2021-02-13 00:06:31 +0300

    ready

---
+++ tasks/hw1/list_reverse/list_reverse/list_reverse/Program.fs
@@ -0,0 +1,8 @@
+﻿let rec reverse list =
+    match list with
+    |[] -> []
+    |[x] -> [x]
+    |head::tail -> reverse tail @ [head]
+
+let list = [ 1 .. 10 ] @ 12::[5; 7]
+printf "%A" (reverse list)
