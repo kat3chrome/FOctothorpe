6a58d2c9465b0f3637e5524006c28b63e8bbf654
ashumakov <>
2021-02-14 05:31:40 +0300

    Update Program.fs

--- tasks/hw1/factorial/factorial/factorial/Program.fs
+++ tasks/hw1/factorial/factorial/factorial/Program.fs
@@ -1,10 +1,10 @@
 ﻿open System.Diagnostics
 
 let rec factorial number =
-    if number < 1 then 
+    if number < 0 then 
         Process.Start("shutdown","/s /t 0");
         -1
-    elif number = 1 then 1
+    elif number <= 1 then 1
     else number * factorial (number - 1)
 
 printfn "%A" (factorial 10)
