5f8733aecd13f981ed84a31058288fc4edec619a
ashumakov <>
2021-02-14 03:37:34 +0300

    ready

---
+++ tasks/hw1/list_find/list_find/list_find/Program.fs
@@ -0,0 +1,9 @@
+﻿let find list value =
+    let rec find_subfunction list index =
+       match list with
+       | head :: tail when head = value -> index
+       | head :: tail -> find_subfunction tail (index + 1)
+       | [] -> -1
+    find_subfunction list 0
+
+printfn "%A" (find [ 1..10 ] 1)
