31e84e9bc3436c982f337476bb937e6b2b4aee84
ashumakov <>
2021-02-14 04:47:45 +0300

    Ready

---
+++ tasks/hw1/2_pow/2_pow/2_pow/Program.fs
@@ -0,0 +1,10 @@
+﻿let generateListOfTwoPowers n m =
+    let rec generateListOfTwoPowersSubfunction list index = 
+        if index <= m + n then
+            let list = list @ [(pown 2 index)]
+            generateListOfTwoPowersSubfunction list (index + 1)
+        else list
+
+    generateListOfTwoPowersSubfunction [] n
+
+printfn "%A" (generateListOfTwoPowers 2 3)
