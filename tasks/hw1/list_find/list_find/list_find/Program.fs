9f1be289be931afbe27dfc89a16f2aaf7bf8cb18
ashumakov <>
2021-02-14 15:00:02 +0300

    Added option

--- tasks/hw1/list_find/list_find/list_find/Program.fs
+++ tasks/hw1/list_find/list_find/list_find/Program.fs
@@ -1,9 +1,9 @@
 ﻿let find list value =
     let rec findSubfunction list index =
        match list with
-       | head :: tail when head = value -> index
+       | head :: tail when head = value -> Some(index)
        | head :: tail -> findSubfunction tail (index + 1)
-       | [] -> -1
+       | [] -> None
     findSubfunction list 0
 
-printfn "%A" (find [ 1..10 ] 1)
+printfn "%A" (find [ 1..10 ] 10)
