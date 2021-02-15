5627cef306bb0dc040889e8bb16fc6787e9bad8f
ashumakov <>
2021-02-15 16:39:27 +0300

    replaced @ to ::

--- tasks/hw1/2_pow/2_pow/2_pow/Program.fs
+++ tasks/hw1/2_pow/2_pow/2_pow/Program.fs
@@ -3,14 +3,12 @@
         failwith $"Incorrect value n = '{n}'"
     elif m < 0 then
         failwith $"Incorrect value m = '{m}'"
-    elif n > m then
-        failwith $"Incorrect values n = '{n}', m = '{m}'"
 
     let rec generateListOfTwoPowersSubfunction list value index = 
         if index <= m + n then
-            generateListOfTwoPowersSubfunction (list @ [value + value]) (value + value) (index + 1)
+            generateListOfTwoPowersSubfunction ((value / 2) :: list) (value / 2) (index + 1)
         else list
 
-    generateListOfTwoPowersSubfunction [] (pown 2 (n - 1)) n
+    generateListOfTwoPowersSubfunction [] (pown 2 (m + n + 1)) n
 
-printfn "%A" (generateListOfTwoPowers 4 10)
+printfn "%A" (generateListOfTwoPowers 3 5)
