5d20b54a41189f59fa7901c82a729f0bc93b213f
kat3chrome <47758828+kat3chrome@users.noreply.github.com>
2021-02-20 21:16:20 +0300

    spell cleanup

--- tasks/hw2/UsingMapAndFilterAndFold/UsingMapAndFilterAndFold/UsingMapAndFilterAndFoldTest/UsingMapAndFilterAndFoldTest.fs
+++ tasks/hw2/UsingMapAndFilterAndFold/UsingMapAndFilterAndFold/UsingMapAndFilterAndFoldTest/UsingMapAndFilterAndFoldTest.fs
@@ -6,15 +6,15 @@ open FsCheck.NUnit
 open main
 
 [<Test>]
-let ``even countin with map that should equal 10`` () =
+let ``Even counting with map that should equal 10`` () =
     evenCountinWithMap [1; 2; 3; -1; 2; 3; 4; 5; 6] |> should equal 4
 
 [<Test>]
-let ``even countin with filter that should equal 10`` () =
+let ``Even counting with filter that should equal 10`` () =
     evenCountinWithFilter [1; 2; 3; -1; 2; 3; 4; 5; 6] |> should equal 4
 
 [<Test>]
-let ``even countin with fold that should equal 10`` () =
+let ``Even counting with fold that should equal 10`` () =
     evenCountinWithFold [1; 2; 3; -1; 2; 3; 4; 5; 6] |> should equal 4
 
 [<Property>]
