78ffc896fc05bbb597da82205ddfafb82615f4e8
kat3chrome <47758828+kat3chrome@users.noreply.github.com>
2021-02-24 15:48:25 +0300

    issues fixed

--- tasks/hw2/MapForTrees/MapForTrees/MapForTrees/MapForTrees/Program.fs
+++ tasks/hw2/MapForTrees/MapForTrees/MapForTrees/MapForTrees/Program.fs
@@ -2,20 +2,23 @@
 // и возвращающую новое двоичное дерево, каждый элемент которого — результат 
 // применения функции к соответствующему элементу исходного дерева (map для деревьев).
 
-module main
+module Main
 
+    /// Binary tree.
     type Tree<'a> =
-        |Tree of 'a * Tree<'a> * Tree<'a>
+        |TreeTwoChild of 'a * Tree<'a> * Tree<'a>
+        |TreeOneChild of 'a * Tree<'a>
         |Leaf of 'a
 
     let rec mapForTree function' tree =
         match tree with
-        |Tree(node, leftChild, rightChild) -> Tree(function' node, mapForTree function' leftChild, mapForTree function' rightChild)
+        |TreeTwoChild(node, leftChild, rightChild) -> TreeTwoChild(function' node, mapForTree function' leftChild, mapForTree function' rightChild)
+        |TreeOneChild(node, child) -> TreeOneChild(function' node, mapForTree function' child)
         |Leaf(node) -> Leaf(function' node)
 
     [<EntryPoint>]
     let main argv =
-        let tree = Tree (1, Tree (2, Leaf 4, Leaf 5), Tree (3, Leaf 6, Leaf 7))
+        let tree = TreeTwoChild (1, TreeOneChild (2, TreeTwoChild (3, Leaf 6, Leaf 7)), TreeTwoChild (3, Leaf 6, Leaf 7))
         printfn "%A" tree
         printfn "%A" (mapForTree (fun x -> x + 1) tree)
         0 
--- tasks/hw2/MapForTrees/MapForTrees/MapForTrees/MapForTreesTest/MapForTreesTest.fs
+++ tasks/hw2/MapForTrees/MapForTrees/MapForTrees/MapForTreesTest/MapForTreesTest.fs
@@ -3,14 +3,10 @@ module MapForTreesTest
 open NUnit.Framework
 open FsUnit
 open FsCheck.NUnit
-open main
+open Main
 
 [<Test>]
 let ``Test mapForTree with function which squares the values`` () =
-    let treeInput = Tree (1, Tree (2, Leaf 4, Leaf 5), Tree (3, Leaf 6, Leaf 7))
-    let treeResult = Tree (1, Tree (4, Leaf 16, Leaf 25), Tree (9, Leaf 36, Leaf 49))
+    let treeInput = TreeTwoChild (1, TreeTwoChild (2, Leaf 4, Leaf 5), TreeTwoChild (3, Leaf 6, Leaf 7))
+    let treeResult = TreeTwoChild (1, TreeTwoChild (4, Leaf 16, Leaf 25), TreeTwoChild (9, Leaf 36, Leaf 49))
     mapForTree (fun x -> x * x) treeInput |> should equal treeResult
-
-[<Property>]
-let ``Checking the stability of a function, that the values are equal for equal trees``(testTree:Tree<int>) =
-    mapForTree (fun x -> x + 1) testTree = mapForTree (fun x -> x + 1) testTree
