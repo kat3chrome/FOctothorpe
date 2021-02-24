02daa54f30975508ef072c5c8343a3aa9b731037
kat3chrome <47758828+kat3chrome@users.noreply.github.com>
2021-02-24 15:27:49 +0300

    issues fixed

--- tasks/hw2/PrimeNumbers/PrimeNumbers/PrimeNumbers/PrimeNumbers/PrimeNumbers/Program.fs
+++ tasks/hw2/PrimeNumbers/PrimeNumbers/PrimeNumbers/PrimeNumbers/PrimeNumbers/Program.fs
@@ -4,15 +4,15 @@
 module main
 
     let isPrime n = 
-        let bound = int(sqrt(float n))
+        let bound = int (sqrt (float n))
         seq{2..bound}
         |> Seq.exists (fun x -> n % x = 0) 
         |> not
 
     let rec nextPrime n = 
         if isPrime (n + 1) then n + 1
-        else nextPrime (n+1)
+        else nextPrime (n + 1)
 
-    let rec sequenceOfPrimes () = Seq.unfold(fun n -> Some(n, nextPrime n)) 1
+    let rec sequenceOfPrimes () = Seq.unfold (fun n -> Some(n, nextPrime n)) 1
 
     printf "%A" (sequenceOfPrimes ())
