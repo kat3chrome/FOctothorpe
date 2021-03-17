da572009d14fde524b0dc6ce96cd181c81648009
ashumakov <>
2021-03-18 00:05:11 +0300

    Done

---
+++ tasks/hw4/BracketSequence/BracketSequence/BracketSequence/Program.fs
@@ -0,0 +1,14 @@
+﻿module BracketSequence
+
+open System
+
+let isCorrectBracketSequence input =
+    let rec loop input = 
+        match input:string with
+        | input when input.Contains "()" -> loop (input.Replace ("()", ""))
+        | input when input.Contains "{}" -> loop (input.Replace ("{}", ""))
+        | input when input.Contains "[]" -> loop (input.Replace ("[]", ""))
+        | _ -> input
+    if (input.Equals "") then false else (loop input).Equals ""
+
+printfn "%A" (isCorrectBracketSequence "([])")
---
+++ tasks/hw4/BracketSequence/BracketSequence/BracketSequenceTest/BracketSequenceTest.fs
@@ -0,0 +1,24 @@
+module BracketSequenceTest
+
+open BracketSequence
+open NUnit.Framework
+
+[<Test>]
+let ``() should be correct`` () =
+    Assert.IsTrue(isCorrectBracketSequence "()")
+
+[<Test>]
+let ``([(){}][[{}]][]()) should be correct`` () =
+    Assert.IsTrue(isCorrectBracketSequence "([(){}][[{}]][]())")
+
+[<Test>]
+let ``([)] should not be correct`` () =
+    Assert.IsFalse(isCorrectBracketSequence "([)]")
+
+[<Test>]
+let ``42 should not be correct`` () =
+    Assert.IsFalse(isCorrectBracketSequence "42")
+
+[<Test>]
+let ``empty string should not be correct`` () =
+    Assert.IsFalse(isCorrectBracketSequence "")
