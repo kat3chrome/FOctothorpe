1c5faa8473e7271cc04ef171e8ff18112d8623d7
ashumakov <>
2021-03-24 22:22:39 +0300

    Done

---
+++ tasks/hw6/stringWorkflow/stringWorkflow/stringWorkflow/Program.fs
@@ -0,0 +1,27 @@
+﻿module stringWorkflow 
+
+    open System
+
+    type calculate() =
+        member this.Bind (n : string, func) =
+            let parsedString = Int32.TryParse(n)
+            match parsedString with
+            | false, _ -> None
+            | true, x -> func x
+
+        member this.Return(x) =
+            Some(x) 
+
+    calculate () {
+        let! x = "1"
+        let! y = "2"
+        let z = x + y
+        return z
+    } |> printf "%A\n"
+
+    calculate () {
+        let! x = "1"
+        let! y = "Ъ"
+        let z = x + y
+        return z
+    } |> printf "%A"
---
+++ tasks/hw6/stringWorkflow/stringWorkflow/stringWorkflowTest/stringWorkflowTest.fs
@@ -0,0 +1,21 @@
+﻿open stringWorkflow
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``"1" + "2" should equal Some(3)`` () =
+    calculate() {
+        let! x = "1"
+        let! y = "2"
+        let z = x + y
+        return z
+    } |> should equal <| Some(3)
+
+[<Test>]
+let ``"1" + "Ъ" should equal None`` () =
+    calculate() {
+        let! x = "1"
+        let! y = "Ъ"
+        let z = x + y
+        return z
+    } |> should equal <| None
