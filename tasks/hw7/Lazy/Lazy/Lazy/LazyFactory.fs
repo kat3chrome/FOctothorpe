526a2910d452218aa9ff1f89c3f2e0031344afdd
ashumakov <>
2021-04-01 22:31:13 +0300

    Basic

---
+++ tasks/hw7/Lazy/Lazy/Lazy/LazyFactory.fs
@@ -0,0 +1,16 @@
+﻿module LazyFactory
+
+open System.Threading
+
+type ILazy<'a> =
+    abstract member Get: unit -> 'a
+
+type SingleMode<'a> (supplier) = 
+    let mutable result = None
+    interface ILazy<'a> with
+        member _.Get() = 
+            if result.IsSome then 
+                result.Value
+            else 
+                result <- Some(supplier ())
+                result.Value
---
+++ tasks/hw7/Lazy/Lazy/Lazy/Program.fs
@@ -0,0 +1,5 @@
+﻿open System
+open LazyFactory
+
+let singleMode = (LazyFactory.SingleMode(fun () -> 7) :> ILazy<int>).Get()
+printf "%A" singleMode
