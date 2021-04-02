f2ab97293b47ea93e0289e7e416c980b11838926
ashumakov <>
2021-04-02 18:32:00 +0300

    Done

---
+++ tasks/test/Square/Square/Square/Program.fs
@@ -0,0 +1,10 @@
+﻿
+
+let printSquare N =
+    printf "%s\n" (String.replicate N "*")
+    printf "%s" (String.replicate (N - 2) 
+                    ("*" + (String.replicate (N - 2) " ") + "*\n"))
+    printf "%s\n" (String.replicate N "*")
+
+printSquare 4
+
