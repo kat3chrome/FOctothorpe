0f89c85abaf18a9975dbe24acb1e73eb28ba98a3
ashumakov <>
2021-04-02 18:42:40 +0300

    added test

--- tasks/test/Square/Square/Square/Program.fs
+++ tasks/test/Square/Square/Square/Program.fs
@@ -1,10 +1,14 @@
 ﻿
+module Square
+
+let generateSquare N = 
+    match N with
+    | _ when N > 1 -> ((String.replicate N "*") + "\n" + (String.replicate (N - 2) 
+                         ("*" + (String.replicate (N - 2) " ") + "*\n")) + (String.replicate N "*"))
+    | _ -> "*"
 
 let printSquare N =
-    printf "%s\n" (String.replicate N "*")
-    printf "%s" (String.replicate (N - 2) 
-                    ("*" + (String.replicate (N - 2) " ") + "*\n"))
-    printf "%s\n" (String.replicate N "*")
+    printf "%s\n" (generateSquare N)
 
-printSquare 4
+printSquare 2
 
