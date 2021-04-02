cfb91af3afafb9c9b1bb636879af3710be310f2a
ashumakov <>
2021-04-02 18:52:06 +0300

    create stack type

---
+++ tasks/test/ThreadSafeStack/ThreadSafeStack/ThreadSafeStack/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+type ThreadSafeStack<'T>() =    
+  let mutable stack : List<'T> = []
+
+  member this.Push value =
+    lock stack (fun () -> 
+      stack <- value::stack)
+
+  member this.Pop() =
+    lock stack (fun () ->
+      match stack with
+      | head::tail ->
+          stack <- tail
+          Some head
+      | [] -> None)
