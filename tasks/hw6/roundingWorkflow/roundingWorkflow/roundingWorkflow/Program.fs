8705310e75b23f8980302924fbfb0423f3e9c51f
Anton Shumakov <47758828+anton-shumakov@users.noreply.github.com>
2021-05-26 23:12:36 +0300

    Code cleanup

--- tasks/hw6/roundingWorkflow/roundingWorkflow/roundingWorkflow/Program.fs
+++ tasks/hw6/roundingWorkflow/roundingWorkflow/roundingWorkflow/Program.fs
@@ -2,14 +2,14 @@
 
     open System
 
-    type rounding(accuracy : int) =
+    type Rounding(accuracy : int) =
         member this.Bind (n : float, func) =
             func (Math.Round(n, accuracy))
 
         member this.Return (n : float) =
             Math.Round(n, accuracy)
 
-    let result = rounding 3 {
+    let result = Rounding 3 {
         let! a = 2.0 / 12.0
         let! b = 3.5
         return a / b
--- tasks/hw6/roundingWorkflow/roundingWorkflow/roundingWorkflowTest/roundingWorkflowTest.fs
+++ tasks/hw6/roundingWorkflow/roundingWorkflow/roundingWorkflowTest/roundingWorkflowTest.fs
@@ -5,14 +5,14 @@ open FsUnit
 
 [<Test>]
 let ``((2.0 / 12.0) / 3.5) with rounding 3 should equal 0.048`` () =
-    rounding 3 {
+    Rounding 3 {
         let! a = 2.0 / 12.0
         let! b = 3.5
         return a / b
-    } |> should equal 0.048
+    } |> should (equalWithin 0.001) 0.048
 
 [<Test>]
 let ``(0.01 + 0.01 with rounding 1 should equal 0`` () =
-    rounding 1 {
+    Rounding 1 {
         return 0.01 + 0.01
-    } |> should equal 0
+    } |> should (equalWithin 0.001) 0
