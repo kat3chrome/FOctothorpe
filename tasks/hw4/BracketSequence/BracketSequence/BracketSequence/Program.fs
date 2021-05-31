872f73da5ff469fbec0dba60e91f0f803664b4c6
Anton Shumakov <47758828+anton-shumakov@users.noreply.github.com>
2021-05-31 17:50:31 +0300

    Change generic stack to list

--- tasks/hw4/BracketSequence/BracketSequence/BracketSequence/Program.fs
+++ tasks/hw4/BracketSequence/BracketSequence/BracketSequence/Program.fs
@@ -1,8 +1,6 @@
 ﻿module BracketSequence
 
-open System.Collections.Generic
-
-// Get a reverse bracket
+/// Get a reverse bracket
 let getOpenBracket bracket =
     match bracket with 
     | '(' -> ')'
@@ -10,25 +8,24 @@ let getOpenBracket bracket =
     | '{' -> '}'
     | _ -> failwith "[ERROR] unknown symbol"
 
-// Check is the bracket a open bracket
+/// Check iп the bracket a open 
 let isOpenBracket bracket =
     match bracket with
-    | '['|'{'|'(' -> true
+    | '[' | '{' | '(' -> true
     | _ -> false
 
-// Check is sequence of brackets a correct
+/// Check if the bracket is an open bracket
 let isCorrectBracketSequence input =
     let sequence = Seq.toList input
 
-    let rec loop subsequence (stack:Stack<char>) =
-        match subsequence with
-        | h :: t when isOpenBracket h ->
-            stack.Push(h)
-            loop t stack
-        | h :: _ when stack.Count > 0 && getOpenBracket (stack.Peek()) <> h -> false
-        | h :: t when stack.Count > 0 && getOpenBracket (stack.Pop()) = h -> loop t stack
-        | [] when stack.Count = 0 -> true
+    let rec loop (subsequence: List<char>) (stack: List<char>) =
+        match subsequence, stack with
+        | (h :: t, _) when isOpenBracket h ->
+            loop t (h :: stack)
+        | (h :: _, _) when stack.Length > 0 && getOpenBracket (stack.Head) <> h -> false
+        | (h :: t, _ :: stackTail) when stack.Length > 0 && getOpenBracket (stack.Head) = h -> loop t stackTail
+        | ([], _) when stack.Length = 0 -> true
         | _ -> false
-    loop sequence (Stack<char>())
+    loop sequence []
 
 printfn "%A" (isCorrectBracketSequence "([]){}")
