dc2d134813214c1886e592a61af49a65854cdb6e
Anton Shumakov <47758828+anton-shumakov@users.noreply.github.com>
2021-05-31 20:28:58 +0300

    code cleanup

--- tasks/hw4/BracketSequence/BracketSequence/BracketSequence/Program.fs
+++ tasks/hw4/BracketSequence/BracketSequence/BracketSequence/Program.fs
@@ -1,24 +1,17 @@
 ﻿module BracketSequence
 
 
-/// Get a reverse bracket
-let getOpenBracket bracket =
-    (Map.ofList [ ('(', ')'); ('[', ']'); ('{', '}') ]).[bracket]
-
-/// Check iп the bracket a open 
-let isOpenBracket bracket =
-    (Map.ofList [ ('(', ')'); ('[', ']'); ('{', '}') ]).ContainsKey bracket
-
 /// Check if the bracket is an open bracket
 let isCorrectBracketSequence input =
+    let brackets = Map.ofList [ ('(', ')'); ('[', ']'); ('{', '}') ]
     let sequence = Seq.toList input
 
     let rec loop (subsequence: List<char>) (stack: List<char>) =
         match subsequence, stack with
-        | (h :: t, _) when isOpenBracket h ->
+        | (h :: t, _) when brackets.ContainsKey h ->
             loop t (h :: stack)
-        | (h :: _, _) when stack.Length > 0 && getOpenBracket (stack.Head) <> h -> false
-        | (h :: t, _ :: stackTail) when stack.Length > 0 && getOpenBracket (stack.Head) = h -> loop t stackTail
+        | (h :: _, _) when stack.Length > 0 && brackets.[stack.Head] <> h -> false
+        | (h :: t, _ :: stackTail) when stack.Length > 0 && brackets.[stack.Head] = h -> loop t stackTail
         | ([], _) when stack.Length = 0 -> true
         | _ -> false
     loop sequence []
