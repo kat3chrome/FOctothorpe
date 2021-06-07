c11d7dae53f3662a81c6e04344b8ade64b1744b9
Anton Shumakov <47758828+anton-shumakov@users.noreply.github.com>
2021-06-07 07:16:31 +0300

    Add solution

---
+++ tasks/Special/PointFree2/PointFree2/PointFree2/Program.fs
@@ -0,0 +1,10 @@
+﻿module Main 
+
+/// Initial expression
+let f g l = List.map g (List.tail l)
+/// Middle expression
+let f'1 g  = List.tail >> List.map g 
+/// Final expression
+let f'2 = ((>>) List.tail) << List.map
+
+f'2 (fun x -> printfn "%A" x) [1; 2; 3] |> ignore
