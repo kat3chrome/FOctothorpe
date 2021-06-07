c17f55d4d648dda43cb5ae104a80b84ef5438508
Anton Shumakov <47758828+anton-shumakov@users.noreply.github.com>
2021-06-07 12:43:04 +0300

    Ready

---
+++ tasks/Special/Multiplication/Multiplication/Multiplication/Program.fs
@@ -0,0 +1,15 @@
+﻿// Реализовать функцию, которая вычисляет произведение цифр данного числа
+
+module Multiplication
+
+//let stringToMultiplication = Seq.toList >> List.map (fun x -> int x - int '0') >> List.fold (*) 1
+
+let multiply input =
+    let rec loop input accumulator =
+        match input with
+        | h::t when List.contains h [0..9] -> loop t (accumulator * h)
+        | [] -> accumulator |> Some
+        | _ -> None
+    loop (Seq.toList input |> List.map (fun x -> int x - int '0')) 1
+
+printf "%A" (multiply "123")
